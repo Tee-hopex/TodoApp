0f27836be5092f7ba146ad74c5adad401e60d2a6
tee-hopex <afolayanthopex@gmail.com>
2025-10-08 13:27:09 +0100

    first commit

---
+++ src/todo/Todo.jsx
@@ -0,0 +1,83 @@
+import {useState} from "react"
+
+import todoList from "./todoList.js"
+
+import './Todo.css'
+
+function Todo() {
+    const [toggleTodo, setToggleTodo] = useState(false);
+    const [formData, setFormData] = useState({})
+
+    function handleToggle() {
+        setToggleTodo(!toggleTodo)
+    }
+
+    function handleInputChange(event) {
+        const name = event.target.name;
+        const value = event.target.value;
+        
+        formData[name] = value
+        formData["isCompleted"] = false
+
+        // console.log(formData)
+    }
+
+    function handleFormSubmit(event){
+        event.preventDefault()
+        todoList.push(formData)
+        console.log("form submitted")
+        setToggleTodo(!toggleTodo)
+        // console.log(formData)
+        setFormData({})
+    }
+    
+
+    return (
+        <>
+        <section className="header-section">
+            <h1 className="main-title">WELCOME TO THE TODO</h1>
+        </section>
+
+        <section className="todo-section">
+            <h1 className="section-title">Todo-List</h1>
+            <button className="add-todo-btn" onClick={handleToggle}>Add todo</button>
+            <div className="todo-list-container">
+                {(todoList.length == 0)? 
+                <h1 className="empty-message">Nothing on your todo</h1> : 
+                todoList.map((item, index) => (
+                    <div className="todo-item" key={index}>
+                        <h3 className="todo-title">{item.title}</h3>
+                        <p className="todo-time">{item.time}</p>
+                        <div className="todo-status" style={{height: "30px", width: "30px", backgroundColor: "green"}}>
+                            {item.isCompleted ? "✓" : "X"} 
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+
+        {
+            toggleTodo? 
+            <section className="form-section">
+                <form className="todo-form" onSubmit={handleFormSubmit}>
+                    <fieldset className="form-fieldset">
+                        <legend className="form-legend">todo info</legend>
+                        <label className="form-label" htmlFor="title">Title</label>
+                        <input className="form-input" onChange={handleInputChange} type="text" id="title" name= "title" />
+                        <label className="form-label" htmlFor="time">time</label>
+                        <input className="form-input" onChange={handleInputChange} type="text" id="time" name="time" />
+
+                        <input className="form-submit" type="submit" />
+                    </fieldset>
+                </form>
+            </section>
+            : 
+            null 
+        }
+
+
+        </>
+    )
+}
+
+export default Todo
---
+++ src/todo/todoList.js
@@ -0,0 +1,15 @@
+const todoList = [
+    {
+        title : "wake up",
+        time : "8am",
+        isCompleted: true,
+    },
+    {
+        title : "brush your teeth",
+        time : "8:30am",
+        isCompleted: true,
+    }
+]
+
+
+export default todoList
